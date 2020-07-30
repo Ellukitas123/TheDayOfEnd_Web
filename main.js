40518eadf944ab0fd6b159cad34a4c0088dec2ae
Ellukitas123 <lucas.winand@hotmail.es>
2020-07-30 17:55:20 +0200

    noticia movil

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector(".nav-menu").classList.toggle('show');
+});
+
+ScrollReveal().reveal(".showcase", {delay:250});
+ScrollReveal().reveal(".cards", {delay:250});
+ScrollReveal().reveal(".cards-banner-two", {delay:250});
+ScrollReveal().reveal(".sectit", {delay:250});
